5ddf76807d54afd3dbe98b4ef928a20c4fd2a692
Ahmedhassano <133208592+Ahmedhassano@users.noreply.github.com>
2023-05-18 18:51:29 +0300

    Add files via upload

---
+++ animation_tab_bar_menu.js
@@ -0,0 +1,5 @@
+let menu_container = document.querySelector(".menu_container")
+let toggle=document.querySelector(".toggle")
+toggle.onclick=function () {
+    menu_container.classList.toggle("active")
+}
